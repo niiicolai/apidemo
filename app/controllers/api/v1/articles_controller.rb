ecfdc33269b0bfcf2089251dd91c19694e1dc1e0
niiicolai <niiicolai@gmail.com>
2015-03-08 13:26:13 +0100

    test

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ArticlesController < ActionController::Base
 	protect_from_forgery with: :null_session
+	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 	before_filter :authenticate
 
 	def index
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::SessionsController < Devise::SessionsController
     protect_from_forgery with: :null_session
+    skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
     before_filter :authenticate, :only => [:destroy]
 
     def create
