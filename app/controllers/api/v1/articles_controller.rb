135a42d75df90627e94980d63bd3f951d855e430
niiicolai <niiicolai@gmail.com>
2015-03-09 22:35:50 +0100

    test

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,6 +3,21 @@ class Api::V1::ArticlesController < ActionController::Base
 	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 	before_filter :authenticate
 
+	def get_token
+      user = User.find_by_email(params[:user][:email])
+      if user.blank?
+        render :json => { :info => "Couldn't find a user with that email"}, :status => 200
+      else
+        if user.valid_password?(params[:user][:password])
+          access_token = SecureRandom.hex
+          current_user.update(:token => access_token)
+          render :json => { :info => "Logged in", :user => current_user }, :status => 200
+        else
+          render :json => { :info => "email or password is wrong"}, :status => 200
+        end
+      end
+    end
+
 	def index
 		render :json => { :info => "authenticated", :article => @user.articles.all }, :status => 200
 	end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,7 @@
 class ArticlesController < ApplicationController
 	before_action :authenticate_user!
 
-	 def create_token
-      user = User.find_by_email(params[:user][:email])
-      if user.blank?
-        render :json => { :info => "Couldn't find a user with that email"}, :status => 200
-      else
-        if user.valid_password?(params[:user][:password])
-          access_token = SecureRandom.hex
-          current_user.update(:token => access_token)
-          render :json => { :info => "Logged in", :user => current_user }, :status => 200
-        else
-          render :json => { :info => "email or password is wrong"}, :status => 200
-        end
-      end
-    end
+	 
 
 	def index
 		@article = current_user.articles.all
